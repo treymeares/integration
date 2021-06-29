a46d1d5f5cceadff904d0925030d663542da05e9
trme3 <treymeares@gmail.com>
2021-06-29 18:34:36 -0400

    Removed secret key

--- integration/settings.py
+++ integration/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from integration.secret_key import secret_key_trey_django
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zp8)#togh)t!g3xwk3%bl6%o#jejkj8+av0(9njtc9ea_lm@k8'
+SECRET_KEY = secret_key_trey_django
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
